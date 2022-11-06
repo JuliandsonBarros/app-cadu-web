1b185afa436b820e2c78ef6c2b7cfd9a31027bdd
Daniel Lopes <cyberlink.daniel@gmail.com>
2022-11-06 02:14:07 -0300

    carga inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,50 @@
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/autentica/login/login.component';
+import { UsuarioCreateComponent } from './components/usuario/usuario-create/usuario-create.component';
+import { UsuarioComponent } from './components/views/usuario/usuario.component';
+import { SecretariaCreateComponent } from './components/secretaria/secretaria-create/secretaria-create.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/views/home/home.component';
+import { SecretariaComponent } from './components/views/secretaria/secretaria.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: "",
+    component: LoginComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+
+  {
+    path: "secretarias",
+    component: SecretariaComponent
+  },
+  {
+    path: "secretaria/create",
+    component: SecretariaCreateComponent
+  },
+  {
+    path: "secretaria/update/:id",
+    component: SecretariaComponent
+  },
+  {
+    path: "secretaria/delete/:id",
+    component: SecretariaComponent
+  },
+  {
+    path: "usuarios",
+    component: UsuarioComponent
+  },
+  {
+    path: "usuario/create",
+    component: UsuarioCreateComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,66 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+
+import { MatCardModule } from  '@angular/material/card';
+import { MatSidenavModule } from  '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from  '@angular/material/button';
+import { MatSnackBarModule } from  '@angular/material/snack-bar';
+import { HttpClientModule } from  '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { SecretariaComponent } from './components/views/secretaria/secretaria.component';
+import { SecretariaCreateComponent } from './components/secretaria/secretaria-create/secretaria-create.component';
+import { SecretariaReadComponent } from './components/secretaria/secretaria-read/secretaria-read.component';
+import { UsuarioComponent } from './components/views/usuario/usuario.component';
+import { UsuarioReadComponent } from './components/usuario/usuario-read/usuario-read.component';
+import { UsuarioCreateComponent } from './components/usuario/usuario-create/usuario-create.component';
+import { LoginComponent } from './components/autentica/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NavComponent,
+    SecretariaComponent,
+    SecretariaCreateComponent,
+    SecretariaReadComponent,
+    UsuarioComponent,
+    UsuarioReadComponent,
+    UsuarioCreateComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
